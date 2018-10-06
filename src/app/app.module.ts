b479a1e82b6403980773a4b1223c71f3e0ed4d2c
subhankar90 <das.subhankar90@gmail.com>
2018-10-06 09:35:53 +0530

    test1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { Test1Component } from './test1/test1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule
